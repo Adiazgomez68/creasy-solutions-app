07717d9c588fcbe99f0c9e28fc5674199905ac22
Ingcruzco <cruzco2009@hotmail.com>
2021-11-17 12:27:49 -0500

    Creacion carpeta base de datos, modelos y archivos

--- routes/users/route.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {userRegistertHandler,userUpdatetHandler,userLoginHandler}=require('../../controller/users/users.controller.js')
-router.post('/user/register',userRegistertHandler)
-router.put('/user/register',userUpdatetHandler)
-router.post('/user/login',userLoginHandler)
-
-
-module.exports=router;
