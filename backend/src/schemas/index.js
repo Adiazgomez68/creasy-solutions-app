c02c3d782f899251b543ae853c344e1911786af8
Andrés Díaz <Adiazgomez68@correo.unicordoba.edu.co>
2021-12-06 19:55:12 -0500

    Updates in file users

--- backend/src/schemas/index.js
+++ backend/src/schemas/index.js
@@ -1,18 +1,17 @@
 const { SchemaComposer } = require('graphql-compose');
 
-
 const schemaComposer = new SchemaComposer();
 
-//const { UserQuery, UserMutation } = require('./user');
+const { UserQuery, UserMutation } = require('./users');
 const { ProjectQuery, ProjectMutation } = require('./projects');
 
 schemaComposer.Query.addFields({
-    //...UserQuery,
+    ...UserQuery,
     ...ProjectQuery,
 });
 
 schemaComposer.Mutation.addFields({
-    //...UserMutation,
+    ...UserMutation,
     ...ProjectMutation,
 });
 
